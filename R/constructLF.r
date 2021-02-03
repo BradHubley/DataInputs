8471497f5122208c6d202e1ef0982dfba29b6503
Hubley <Brad.Hubley@dfo-mpo.gc.ca>
2021-02-03 20:51:32 +0000

    modified script to reflect new function

--- R/constructLF.r
+++ R/constructLF.r
@@ -1,13 +1,15 @@
-constructLF<-function(bins,Yrs,sx=c(1,2,0)){
+constructLF<-function(LFdata, bins,Yrs,sx=c(1,2,0)){
 
   LF=list()
   bins = paste0("L",bins[-1])
-  FixedSurvey$N_MEASURED <- rowSums(FixedSurvey[,bins])
+  LFdata$N_MEASURED <- rowSums(LFdata[,bins])
 
   for(i in 1:length(Yrs)){
 
-     LF[[i]]=t(sapply(sx,function(s){colSums(subset(FixedSurvey,YEAR==Yrs[i]&SEXCD_ID==s,bins),na.rm=T)}))
+     LF[[i]]=t(sapply(sx,function(s){colSums(subset(LFdata,YEAR==Yrs[i]&SEXCD_ID==s,bins),na.rm=T)}))
 
   }
+  names(LF) = Yrs
+
   return(LF)
 }
