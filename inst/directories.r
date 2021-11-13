a875cb5737f151d3d355a97cdb48c7880bc6e535
BradHubley <brad.hubley@dfo-mpo.gc.ca>
2021-11-13 20:42:07 +0000

    directories

--- inst/directories.r
+++ inst/directories.r
@@ -1,4 +1,5 @@
 datadir="C:/Users/hubleyb/Documents/Halibut/data"
-wd="C:/Users/hubleyb/Documents/Halibut/Git/DataInputs/"
+wd="C:/Users/hubleyb/Documents/Halibut/Git/DataInputs"
 #datadir="C:/Users/harperd/Documents/Halibut/RDataVault"
 #wd="C:/Users/harperd/Documents/Halibut/GitDataInputs/DataInputs/"
+
