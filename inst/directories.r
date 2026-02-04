7a5d18c40cedf09bdf1697fff9ba2d47e7ce7fb9
DanniHarper <danni.harper@dfo-mpo.gc.ca>
2026-02-04 18:18:30 +0000

    thanks Lingbo in directory

--- inst/directories.r
+++ inst/directories.r
@@ -1,2 +1,2 @@
-datadir="C:/Users/LiLi/Documents/data"
-wd="C:/Users/LiLi/Documents/DataInputs/"
+datadir="C:/Users/harperd/Documents/Halibut/RDataVault"
+wd="C:/Users/harperd/Documents/Halibut/GitDataInputs/DataInputs_2023/DataInputs"
